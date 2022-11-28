d87b1360892a4c0f86415cb7acff33b5499d1972
FranciosoDonato <franciosodonatowork@gmail.com>
2022-11-28 11:23:07 +0100

    Update

--- web/main.dart
+++ web/main.dart
@@ -20,32 +20,30 @@ const double pi = 3.1415926535897932;
      if (lines[i].length > maxCharsPerLine) maxCharsPerLine = lines[i].length;
    }
 
-   //Recall function drawlines for each line
-   for (int i = 0; i < nLines; i++) {
-    //  drawLines(canvas.width, canvas.height, ctx, nLines, maxCharsPerLine, i, lines[i]);
-    drawCircle(canvas.width, canvas.height, ctx, nLines, maxCharsPerLine, i, lines[i]);
-  }//
+    drawCircle(canvas.width, canvas.height, ctx, nLines, maxCharsPerLine, lines);
  }
 
-//Draw line calculating for each time height and width
-void drawLines(var width, var height, CanvasRenderingContext2D ctx,
-    int nLines, int maxCharsPerLine, int linenumber, String line){
-  int posY = height ~/ nLines * (linenumber + 1);
-  var segmentLength = width / maxCharsPerLine;
+ void drawCircle(var width, var height, CanvasRenderingContext2D ctx,
+   int nLines, int maxCharsPerLine, var lines){
+   var segmentLength = width / maxCharsPerLine;
+   var check = 0;
+   num lun = 0;
+   List<Point> punti = [];
+   createPoint(punti);
+   for(int i=0; i < punti.length;i++){
+     check = lines[i].length;
+     lun = width/ check;
+     print("W " + width.toString());
+     print("C " + check.toString());
+     print("L " + lun.toString());
+     drawLine(punti[i].x, punti[i].y, segmentLength, lun);
+   }
+ }
 
-  for (int i = 0; i < line.length; i++){
-    int posX = (segmentLength * i).toInt();
-    if (line[i] != " ") drawLine(posX, posY, segmentLength);
-  }
-}
-
-void drawLine(x, y, len){
-  ctx
-    ..lineWidth = 3
-  // ..globalAlpha = .2
-      ;
+void drawLine(x, y, maxlen, len){
   ctx
     ..beginPath()
+    ..lineWidth = 3
     ..moveTo(x, y)
     ..lineTo(x + len, y)
     ..lineCap = 'round'
@@ -54,71 +52,19 @@ void drawLine(x, y, len){
   ;
 }
 
-// void cerchio(num x, num y, len){
-//   var centro = Point(500,500);
-//   var p;
-//   //num x = 0;
-//   //num y = 0;
-//   for(num i=0; i<360; i = i + 3){
-//     if(i%2==0)
-//     {
-//       x = centro.x + 400 * cos((i*pi)/180);
-//       y = centro.y + 400 * sin((i*pi)/180);
-//       p = Point(x,y);
-//       ctx
-//         ..beginPath()
-//         ..rect(p.x,p.y,len+10,2)
-//         ..stroke()
-//         ..closePath();
-//     }
-//   }
-// }
-
-void drawCircle(var width, var height, CanvasRenderingContext2D ctx,
-    int nLines, int maxCharsPerLine, int linenumber, String line){
+void createPoint(var punti){
   var centro = Point(500,500);
-  var segmentLength = width / maxCharsPerLine;
-
-  var p;
   num x = 0;
   num y = 0;
-  // x = centro.x + 250 * cos(((360)*pi)/180);
-  // y = centro.y + 250 * sin(((360)*pi)/180);
-  // p = Point(x,y);
-  // ctx
-  //   ..beginPath()
-  //   ..rect(p.x,p.y,2,2)
-  //   ..stroke()
-  //   ..closePath();
-  for(num i=0.0; i<360; i = i + 3){
-    // if((70<i && i<110 && i%2==0)){
-    //   x = centro.x + 250 * cos(((i)*pi)/180);
-    //   y = centro.y + 250 * sin(((i)*pi)/180);
-    //   p = Point(x,y);
-    //   ctx
-    //     ..beginPath()
-    //     ..rect(p.x,p.y,20,2)
-    //     ..stroke()
-    //     ..closePath();
-    // }else if(70<i && i<110){
-    //
-    // }else
-    if(i%2==0)
-    {
-    x = centro.x + 400 * cos((i*pi)/180);
-    y = centro.y + 400 * sin((i*pi)/180);
-    p = Point(x,y);
-    drawLine(x, y, segmentLength);
-    // ctx
-    //   ..beginPath()
-    //   ..fillText("Ciao", p.x, p.y)
-    //   ..rect(p.x,p.y,25,2)
-    //   ..stroke()
-    //   ..closePath();
-      }
-      }
+  for(num i = 0.0; i<360; i = i+3)
+  {
+    if(i%2==0){
+      x = centro.x + 400 * cos((i*pi)/180);
+      y = centro.y + 400 * sin((i*pi)/180);
+      punti.add(Point(x,y));
+    }
+  }
 }
-
 
 void main() {
 //path csv
